f28281c8def07e09645ae9c80eb3fa5a998b25ec
aditraglia <0035361@studentmail.unicas.it>
2017-11-21 16:17:24 +0100

    Classe SensingElement (solo parametro name)

--- src/model/SensingElement.java
+++ src/model/SensingElement.java
@@ -1,7 +1,34 @@
 package model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import sun.plugin2.os.windows.SECURITY_ATTRIBUTES;
+
 /**
  * Created by Antonio on 21/11/2017.
  */
 public class SensingElement {
+
+    private StringProperty SE_name;
+
+    //Default constructor
+    public SensingElement(){
+        this.SE_name=null;
+    }
+
+    public SensingElement(String SE_name){
+        this.SE_name=new SimpleStringProperty(SE_name);
+    }
+
+    public String getSE_name() {
+        return SE_name.get();
+    }
+
+    public StringProperty SE_nameProperty() {
+        return SE_name;
+    }
+
+    public void setSE_name(String SE_name) {
+        this.SE_name.set(SE_name);
+    }
 }
