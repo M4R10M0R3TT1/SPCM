cab6ac8c36b1f0e5ce575fdb4fb8fe45c6202396
aditraglia <0035361@studentmail.unicas.it>
2017-11-28 22:39:27 +0100

    ComboBox

--- src/it/unicas/sensiplusConfigurationManager/view/SEEditDialogController.java
+++ src/it/unicas/sensiplusConfigurationManager/view/SEEditDialogController.java
@@ -67,6 +67,30 @@ public class SEEditDialogController {
     @FXML
     private ComboBox rSenseComboBox;
     @FXML
+    private ComboBox inGainComboBox;
+    @FXML
+    private ComboBox outGainComboBox;
+    @FXML
+    private ComboBox contactsComboBox;
+    @FXML
+    private ComboBox harmonicComboBox;
+    @FXML
+    private ComboBox modeVIComboBox;
+    @FXML
+    private ComboBox measureTechniqueComboBox;
+    @FXML
+    private ComboBox measureTypeComboBox;
+    @FXML
+    private ComboBox phaseShiftModeComboBox;
+    @FXML
+    private ComboBox IQComboBox;
+    @FXML
+    private ComboBox inPortADCComboBox;
+    @FXML
+    private ComboBox measureUnitComboBox;
+
+
+    @FXML
     private Label seNameLabel;
 
     private Stage dialogStage;
@@ -76,9 +100,54 @@ public class SEEditDialogController {
 
     @FXML
     private void initialize() {
+        //rSense ComboBox
         rSenseComboBox.getItems().addAll(50,500,5000,50000);
-        rSenseComboBox.setValue(50);
         rSenseComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //inGain ComboBox
+        inGainComboBox.getItems().addAll(1,12,20,40);
+        inGainComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //outGain ComboBox
+        outGainComboBox.getItems().addAll(0,1,2,3,4,5,6,7);
+        outGainComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //contacts ComboBox
+        contactsComboBox.getItems().addAll("TWO","FOUR");
+        contactsComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //harmonic ComboBox
+        harmonicComboBox.getItems().addAll("FIRST_HARMONIC","SECOND_HARMONIC","TIRD_HARMONIC");
+        harmonicComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //modeVI ComboBox
+        modeVIComboBox.getItems().addAll("VOUT_IIN","VIN_IIN","VOUT_VIN","VOUT_VOUT");
+        modeVIComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //measureTechnique ComboBox
+        measureTechniqueComboBox.getItems().addAll("DIRECT, EIS","POT","ENERGY_SPECTROSCOPY","ULTRASOUND");
+        measureTechniqueComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //measureType ComboBox
+        measureTypeComboBox.getItems().addAll("IN-PHASE","QUADRATURE","MODULE","PHASE","RESISTANCE","CAPACITANCE","INDUCTANCE");
+        measureTypeComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //phaseShiftMode ComboBox
+        phaseShiftModeComboBox.getItems().addAll("QUADRANT","COARSE","FINE");
+        phaseShiftModeComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //IQ ComboBox
+        IQComboBox.getItems().addAll("IN_PHASE","QUADRATURE");
+        IQComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //inPortADC ComboBox
+        inPortADCComboBox.getItems().addAll("IA","VA");
+        inPortADCComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
+        //measureUnit ComboBox
+        measureUnitComboBox.getItems().addAll("O","F","H","C","%","V","A","L","t");
+        measureUnitComboBox.valueProperty().addListener((ObservableValue observable,Object oldValue,Object newValue)->newValue.toString());
+
     }
 
     /**
@@ -110,29 +179,30 @@ public class SEEditDialogController {
         else
             seNameLabel.setText("Insert a new SensingElement");
 
-//        rSenseTextField.setText(Integer.toString(sensingElement.getrSense()));
-        inGainTextField.setText(Integer.toString(sensingElement.getInGain()));
-        outGainTextField.setText(Integer.toString(sensingElement.getOutGain()));
-        contactsTextField.setText(sensingElement.getContacts());
+        rSenseComboBox.setValue(sensingElement.getrSense());
+        inGainComboBox.setValue(sensingElement.getInGain());
+        outGainComboBox.setValue(sensingElement.getOutGain());
+        contactsComboBox.setValue(sensingElement.getContacts());
         frequencyTextField.setText(Integer.toString(sensingElement.getFrequency()));
-        harmonicTextField.setText(sensingElement.getHarmonic());
+        harmonicComboBox.setValue(sensingElement.getHarmonic());
         dcBiasTextField.setText(Integer.toString(sensingElement.getDcBias()));
-        modeVITextField.setText(sensingElement.getModeVI());
-        measureTechniqueTextField.setText(sensingElement.getmeasureTechnique());
-        measureTypeTextField.setText(sensingElement.getMeasureType());
+        modeVIComboBox.setValue(sensingElement.getModeVI());
+        measureTechniqueComboBox.setValue(sensingElement.getmeasureTechnique());
+        measureTypeComboBox.setValue(sensingElement.getMeasureType());
         filterTextField.setText(Integer.toString(sensingElement.getFilter()));
-        phaseShiftModeTextField.setText(sensingElement.getPhaseShiftMode());
+        phaseShiftModeComboBox.setValue(sensingElement.getPhaseShiftMode());
         phaseShiftTextField.setText(Integer.toString(sensingElement.getPhaseShift()));
-        IQTextField.setText(sensingElement.getIq());
+        IQComboBox.setValue(sensingElement.getIq());
         conversionTextField.setText(Integer.toString(sensingElement.getConversionRate()));
-        inPortADCTextField.setText(sensingElement.getInPortADC());
+        inPortADCComboBox.setValue(sensingElement.getInPortADC());
         nDataTextField.setText(Integer.toString(sensingElement.getnData()));
         nameTextField.setText(sensingElement.getName());
         rangeMinTextField.setText(Double.toString(sensingElement.getRangeMin()));
         rangeMaxTextField.setText(Double.toString(sensingElement.getRangeMax()));
         defaultAlarmThresholdTextField.setText((Double.toString(sensingElement.getDefaultAlarmThreshold())));
         multiplerTextField.setText(Integer.toString(sensingElement.getMultiplier()));
-        measureUnitTextField.setText(sensingElement.getMeasureUnit());
+        measureUnitComboBox.setValue(sensingElement.getMeasureUnit());
+
     }
 
     /**
@@ -152,29 +222,28 @@ public class SEEditDialogController {
         if (isInputValid(verifyLen)) {
             sensingElement.setIdSensingElement(sensingElementNameTextField.getText());
             sensingElement.setrSense(Integer.parseInt(rSenseComboBox.getValue().toString()));
-            //System.out.println("Valore di rsense: "+Integer.parseInt(rSenseComboBox.getValue().toString()));
-            sensingElement.setInGain(Integer.parseInt(inGainTextField.getText()));
-            sensingElement.setOutGain(Integer.parseInt(outGainTextField.getText()));
-            sensingElement.setContacts(contactsTextField.getText());
+            sensingElement.setInGain(Integer.parseInt(inGainComboBox.getValue().toString()));
+            sensingElement.setOutGain(Integer.parseInt(outGainComboBox.getValue().toString()));
+            sensingElement.setContacts(contactsComboBox.getValue().toString());
             sensingElement.setFrequency(Integer.parseInt(frequencyTextField.getText()));
-            sensingElement.setHarmonic(harmonicTextField.getText());
+            sensingElement.setHarmonic(harmonicComboBox.getValue().toString());
             sensingElement.setDcBias(Integer.parseInt(dcBiasTextField.getText()));
-            sensingElement.setModeVI(modeVITextField.getText());
-            sensingElement.setmeasureTechnique(measureTechniqueTextField.getText());
-            sensingElement.setMeasureType(measureTypeTextField.getText());
+            sensingElement.setModeVI(modeVIComboBox.getValue().toString());
+            sensingElement.setmeasureTechnique(measureTechniqueComboBox.getValue().toString());
+            sensingElement.setMeasureType(measureTypeComboBox.getValue().toString());
             sensingElement.setFilter(Integer.parseInt(frequencyTextField.getText()));
-            sensingElement.setPhaseShiftMode(phaseShiftModeTextField.getText());
+            sensingElement.setPhaseShiftMode(phaseShiftModeComboBox.getValue().toString());
             sensingElement.setPhaseShift(Integer.parseInt(phaseShiftTextField.getText()));
-            sensingElement.setIq(IQTextField.getText());
+            sensingElement.setIq(IQComboBox.getValue().toString());
             sensingElement.setConversionRate(Integer.parseInt(conversionTextField.getText()));
-            sensingElement.setInPortADC(inPortADCTextField.getText());
+            sensingElement.setInPortADC(inPortADCComboBox.getValue().toString());
             sensingElement.setnData(Integer.parseInt(nDataTextField.getText()));
             sensingElement.setName(nameTextField.getText());
             sensingElement.setRangeMin(Double.parseDouble(rangeMinTextField.getText()));
             sensingElement.setRangeMax(Double.parseDouble(rangeMaxTextField.getText()));
             sensingElement.setDefaultAlarmThreshold(Double.parseDouble(defaultAlarmThresholdTextField.getText()));
             sensingElement.setMultiplier(Integer.parseInt(multiplerTextField.getText()));
-            sensingElement.setMeasureUnit(measureUnitTextField.getText());
+            sensingElement.setMeasureUnit(measureUnitComboBox.getValue().toString());
 
             okClicked = true;
             dialogStage.close();
