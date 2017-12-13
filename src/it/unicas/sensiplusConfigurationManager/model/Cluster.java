e0d09badfd68a51156375e52ee47a04e89f3b727
FerdamDT <0036260@studentmail.unicas.it>
2017-12-13 11:18:26 +0100

    classe Cluster completata!

--- src/it/unicas/sensiplusConfigurationManager/model/Cluster.java
+++ src/it/unicas/sensiplusConfigurationManager/model/Cluster.java
@@ -1,5 +1,7 @@
 package it.unicas.sensiplusConfigurationManager.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -8,52 +10,152 @@ import javafx.beans.property.StringProperty;
  */
 public class Cluster {
 
-    private StringProperty IdCluster; //Id o id?
-    private StringProperty _idSerial;
-    private StringProperty _seName;
+    private StringProperty idCluster;
+    private IntegerProperty idCalibration;
+    private StringProperty nameCalibration;
+    private IntegerProperty idConfiguration;
+    private StringProperty driver;
+    private StringProperty hostController;
+    private StringProperty apiOwner;
+    private StringProperty mcu;
+    private StringProperty protocol;
+    private StringProperty addressingType;
+
 
     //Default constructor
-    public Cluster(){this(null,null,null);}
+    public Cluster(){this(null,0,null,0,null,null,null,null,null,null);}
 
-    public Cluster(String IdCluster,String _idSerial,String _seName){
-        this.IdCluster=new SimpleStringProperty(IdCluster);
-        this._idSerial=new SimpleStringProperty(_idSerial);
-        this._seName=new SimpleStringProperty(_seName);
+    public Cluster(String IdCluster,Integer idCalibration,String nameCalibration, Integer idConfiguration,String driver,
+                   String hostController, String apiOwner, String mcu, String protocol, String addressingType){
+        this.idCluster=new SimpleStringProperty(IdCluster);
+        this.idCalibration= new SimpleIntegerProperty(idCalibration);
+        this.nameCalibration= new SimpleStringProperty(nameCalibration);
+        this.idConfiguration= new SimpleIntegerProperty(idConfiguration);
+        this.driver= new SimpleStringProperty(driver);
+        this.hostController= new SimpleStringProperty(hostController);
+        this.apiOwner= new SimpleStringProperty(apiOwner);
+        this.mcu=new SimpleStringProperty(mcu);
+        this.protocol=new SimpleStringProperty(protocol);
+        this.addressingType=new SimpleStringProperty(addressingType);
     }
 
     public String getIdCluster() {
-        return IdCluster.get();
+        return idCluster.get();
     }
 
     public StringProperty idClusterProperty() {
-        return IdCluster;
+        return idCluster;
     }
 
     public void setIdCluster(String idCluster) {
-        this.IdCluster.set(idCluster);
+        this.idCluster.set(idCluster);
     }
 
-    public String get_idSerial() {
-        return _idSerial.get();
+    public int getIdCalibration() {
+        return idCalibration.get();
     }
 
-    public StringProperty _idSerialProperty() {
-        return _idSerial;
+    public IntegerProperty idCalibrationProperty() {
+        return idCalibration;
     }
 
-    public void set_idSerial(String _idSerial) {
-        this._idSerial.set(_idSerial);
+    public void setIdCalibration(int idCalibration) {
+        this.idCalibration.set(idCalibration);
     }
 
-    public String get_seName() {
-        return _seName.get();
+    public String getNameCalibration() {
+        return nameCalibration.get();
     }
 
-    public StringProperty _seNameProperty() {
-        return _seName;
+    public StringProperty nameCalibrationProperty() {
+        return nameCalibration;
     }
 
-    public void set_seName(String _seName) {
-        this._seName.set(_seName);
+    public void setNameCalibration(String nameCalibration) {
+        this.nameCalibration.set(nameCalibration);
+    }
+
+    public int getIdConfiguration() {
+        return idConfiguration.get();
+    }
+
+    public IntegerProperty idConfigurationProperty() {
+        return idConfiguration;
+    }
+
+    public void setIdConfiguration(int idConfiguration) {
+        this.idConfiguration.set(idConfiguration);
+    }
+
+    public String getDriver() {
+        return driver.get();
+    }
+
+    public StringProperty driverProperty() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver.set(driver);
+    }
+
+    public String getHostController() {
+        return hostController.get();
+    }
+
+    public StringProperty hostControllerProperty() {
+        return hostController;
+    }
+
+    public void setHostController(String hostController) {
+        this.hostController.set(hostController);
+    }
+
+    public String getApiOwner() {
+        return apiOwner.get();
+    }
+
+    public StringProperty apiOwnerProperty() {
+        return apiOwner;
+    }
+
+    public void setApiOwner(String apiOwner) {
+        this.apiOwner.set(apiOwner);
+    }
+
+    public String getMcu() {
+        return mcu.get();
+    }
+
+    public StringProperty mcuProperty() {
+        return mcu;
+    }
+
+    public void setMcu(String mcu) {
+        this.mcu.set(mcu);
+    }
+
+    public String getProtocol() {
+        return protocol.get();
+    }
+
+    public StringProperty protocolProperty() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol.set(protocol);
+    }
+
+    public String getAddressingType() {
+        return addressingType.get();
+    }
+
+    public StringProperty addressingTypeProperty() {
+        return addressingType;
+    }
+
+    public void setAddressingType(String addressingType) {
+        this.addressingType.set(addressingType);
     }
 }
